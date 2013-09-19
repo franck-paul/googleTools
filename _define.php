b588dd9419e77e3aa93fb9bbe341072e1029fa29
franck <carnet.franck.paul@gmail.com>
2013-09-19 13:27:18 +0200

    various

--- _define.php
+++ _define.php
@@ -1,18 +1,15 @@
 <?php
-/***************************************************************\
- *  This is 'Google Stuff', a plugin for Dotclear 2            *
- *                                                             *
- *  Copyright (c) 2013                                         *
- *  xave and contributors.                                     *
- *                                                             *
- *  This is an open source software, distributed under the GNU *
- *  General Public License (version 2) terms and  conditions.  *
- *                                                             *
- *  You should have received a copy of the GNU General Public  *
- *  License along with 'My Favicon' (see COPYING.txt);         *
- *  if not, write to the Free Software Foundation, Inc.,       *
- *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    *
-\***************************************************************/
+# -- BEGIN LICENSE BLOCK ----------------------------------
+# This file is part of googleTools, a plugin for Dotclear 2.
+#
+# Copyright (c) xave and contributors
+#
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# -- END LICENSE BLOCK ------------------------------------
+
+if (!defined('DC_RC_PATH')) { return; }
 
 $this->registerModule(
 	/* Name */			"Google Tools",
