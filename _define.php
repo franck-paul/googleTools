8d8fe2aad521b75fd93322f0d7bdfd4f2f283b21
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:44:03 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,21 +10,25 @@
  * @copyright xave
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-
-if (!defined('DC_RC_PATH')) {return;}
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "Google Tools",                                       // Name
-    "Handles Google tools (Analytics & Webmaster Tools)", // Description
-    "xave",                                               // Author
+    'Google Tools',                                       // Name
+    'Handles Google tools (Analytics & Webmaster Tools)', // Description
+    'xave',                                               // Author
     '0.8',                                                // Version
     [
         'requires'    => [['core', '2.17']],                           // Dependencies
         'permissions' => 'contentadmin',                               // Permissions
         'type'        => 'plugin',                                     // Type
-        'support'     => 'https://github.com/franck-paul/googleTools', // Support URL
         'settings'    => [
             'blog' => '#params.google-tools'
-        ]
+        ],
+
+        'details'    => 'https://open-time.net/?q=googleTools',       // Details URL
+        'support'    => 'https://github.com/franck-paul/googleTools', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/googleTools/main/dcstore.xml'
     ]
 );
