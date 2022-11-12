fe7d385b69ef93a01eed3c67e4a407b2db49248a
franck <carnet.franck.paul@gmail.com>
2022-11-12 09:19:56 +0100

    GA js code review, closes #1

--- _public.php
+++ _public.php
@@ -14,11 +14,9 @@ if (!defined('DC_RC_PATH')) {
     return;
 }
 
-dcCore::app()->addBehavior('publicHeadContent', ['googlestuffPublicBehaviours', 'publicHeadContent']);
-
 class googlestuffPublicBehaviours
 {
-    public static function publicHeadContent($core = null)
+    public static function publicHeadContent()
     {
         $res = '';
 
@@ -27,13 +25,16 @@ class googlestuffPublicBehaviours
         }
 
         if (dcCore::app()->blog->settings->googlestuff->googlestuff_uacct != '') {
-            $res .= dcUtils::jsJson('googletools_ga', ['uacct' => dcCore::app()->blog->settings->googlestuff->googlestuff_uacct]) .
-            dcUtils::jsModuleLoad('googleTools/js/ga.js');
+            $uacct = dcCore::app()->blog->settings->googlestuff->googlestuff_uacct;
+
+            $res .= dcUtils::jsJson('googletools_ga', ['uacct' => $uacct]) .
+                '<script async src="https://www.googletagmanager.com/gtag/js?id=' . $uacct . '"></script>' .
+                dcUtils::jsModuleLoad('googleTools/js/ga.js');
 
             if (dcCore::app()->blog->settings->googlestuff->cnil_cookies) {
                 // Includes French CNIL consent check if required
                 $res .= dcUtils::jsJson('googletools_cnil', [
-                    'uacct'  => dcCore::app()->blog->settings->googlestuff->googlestuff_uacct,
+                    'uacct'  => $uacct,
                     'query'  => __('This site use Google Analytics cookies in order to tracking visits. If you want to avoid this, click <a href="javascript:gaOptout()">here</a>.'),
                     'denied' => __('No Google Analytics cookies will be created for tracking your visits on this site.'),
                 ]) .
@@ -44,3 +45,5 @@ class googlestuffPublicBehaviours
         echo $res;
     }
 }
+
+dcCore::app()->addBehavior('publicHeadContent', [googlestuffPublicBehaviours::class, 'publicHeadContent']);
--- js/ga.js
+++ js/ga.js
@@ -1,26 +1,6 @@
-/*global dotclear, ga */
+/*global dotclear */
 'use strict';
 
-{
-  let a = undefined;
-  let m = undefined;
-  const i = window;
-  const s = document;
-  const o = 'script';
-  const g = '//www.google-analytics.com/analytics.js';
-  const r = 'ga';
-  i.GoogleAnalyticsObject = r;
-  (i[r] =
-    i[r] ||
-    function () {
-      (i[r].q = i[r].q || []).push(arguments);
-    }),
-    (i[r].l = 1 * new Date());
-  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-  a.async = 1;
-  a.src = g;
-  m.parentNode.insertBefore(a, m);
-}
-
-ga('create', dotclear.getData('googletools_ga'), 'auto');
-ga('send', 'pageview');
+const ga_data = dotclear.getData('googletools_ga');
+window.dataLayer = window.dataLayer || [];
+dataLayer.push('js', new Date(), 'config', ga_data.uacct);
