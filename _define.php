6756742f780ad087fef874607608a3324f50fd68
franck <carnet.franck.paul@gmail.com>
2023-08-25 09:19:40 +0200

    New release 3.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Google Tools',
     'Handles Google tools (Analytics & Webmaster Tools)',
     'xave',
-    '2.0.1',
+    '3.0',
     [
         'requires'    => [['core', '2.27'], ['php', '8.1']],
         'permissions' => dcCore::app()->auth->makePermissions([
