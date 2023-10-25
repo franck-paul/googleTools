ccda941be3a00dc3bdd763cf594f2dd46c139127
franck <carnet.franck.paul@gmail.com>
2023-10-25 12:14:11 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Google Tools',
     'Handles Google tools (Analytics & Webmaster Tools)',
     'xave',
-    '3.0',
+    '4.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
