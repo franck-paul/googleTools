16d74e3a36982ab578ef2a6fa6e3f408081b2c55
franck <carnet.franck.paul@gmail.com>
2026-02-07 10:52:33 +0100

    Add German and Spanish locales

---
+++ locales/de/main.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Google Tools'] = 'Google-Tools';
+L10n::$locales['Handles Google tools (Analytics & Webmaster Tools)'] = 'Umgang mit Google-Tools (Analytics & Webmaster Tools)';
+L10n::$locales['Google Stuff'] = 'Google-Sachen';
+L10n::$locales['Google Analytics UACCT (ID):'] = 'Google Analytics UACCT (ID):';
+L10n::$locales['Google Webmaster Tools verification:'] = 'Überprüfung durch Google Webmaster Tools:';
+L10n::$locales['Includes CNIL consent for Google Analytics tracking cookies'] = 'Enthält die CNIL-Zustimmung für Google Analytics-Tracking-Cookies';
+L10n::$locales['This site use Google Analytics cookies in order to tracking visits. If you want to avoid this, click <a href="javascript:gaOptout()">here</a>.'] = 'Diese Website verwendet Google Analytics Cookies, um Besuche zu verfolgen. Wenn Sie dies vermeiden möchten, klicken Sie <a href="javascript:gaOptout()">hier</a>.';
+L10n::$locales['No Google Analytics cookies will be created for tracking your visits on this site.'] = 'Es werden keine Google-Analytics-Cookies zur Verfolgung Ihrer Besuche auf dieser Website erstellt.';
---
+++ locales/es/main.lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Google Tools'] = 'Herramientas de Google';
+L10n::$locales['Handles Google tools (Analytics & Webmaster Tools)'] = 'Maneja las herramientas de Google (Analytics & Webmaster Tools)';
+L10n::$locales['Google Stuff'] = 'Cosas de Google';
+L10n::$locales['Google Analytics UACCT (ID):'] = 'Google Analytics UACCT (ID):';
+L10n::$locales['Google Webmaster Tools verification:'] = 'Verificación de Google Webmaster Tools:';
+L10n::$locales['Includes CNIL consent for Google Analytics tracking cookies'] = 'Incluye el consentimiento de la CNIL para las cookies de seguimiento de Google Analytics';
+L10n::$locales['This site use Google Analytics cookies in order to tracking visits. If you want to avoid this, click <a href="javascript:gaOptout()">here</a>.'] = 'Este sitio utiliza cookies de Google Analytics para realizar un seguimiento de las visitas. Si desea evitarlo, haga clic <a href="javascript:gaOptout()">aquí</a>.';
+L10n::$locales['No Google Analytics cookies will be created for tracking your visits on this site.'] = 'No se crearán cookies de Google Analytics para el seguimiento de sus visitas en este sitio.';
